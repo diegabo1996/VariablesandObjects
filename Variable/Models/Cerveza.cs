3063b7241479825a5456a80cf66764bd9140c858
Diego Gabriel León Peña <diegabo1996@yahoo.es>
2022-06-21 19:02:49 -0400

    Agregar archivos de proyecto.

---
+++ Variable/Models/Cerveza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable.Models
+{
+    public class Cerveza
+    {
+        public string Name { get; set; }
+        public string Material { get; set; }
+        public double Price { get; set; }
+        public byte Grade { get; set; }
+        public bool Alocoholic { get; set; }
+    }
+}
---
+++ Variable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Variable.Models;
+namespace Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerveza cerveza = new Cerveza();
+            Console.WriteLine("Como deseas nombrar la cerveza?");
+            cerveza.Name = Console.ReadLine();
+            Console.WriteLine("Que presentacion es la cerveza "+cerveza.Name+"?");
+            cerveza.Material=Console.ReadLine();
+            Console.WriteLine("Cual es el grado alcoholico de la cerveza " + cerveza.Name + "?");
+            cerveza.Grade= byte.Parse(Console.ReadLine());
+            Console.WriteLine("Cual es el precio de la cerveza " + cerveza.Name + "?");
+            cerveza.Price = double.Parse(Console.ReadLine());
+            Console.WriteLine("La cerveza " + cerveza.Name + " es alcoholica?");
+            string rstl = Console.ReadLine();
+            if(rstl=="SI")
+            {
+                cerveza.Alocoholic = true;
+            }
+            else if(rstl=="NO")
+            {
+                cerveza.Alocoholic = false;
+            }
+            Console.WriteLine("Nombre de la Cerveza: " + cerveza.Name);
+            Console.WriteLine("Presentacion: " + cerveza.Material);
+            Console.WriteLine("Grado Alcoholico: " + cerveza.Grade);
+            Console.WriteLine("Precio (Bs): " + cerveza.Price);
+            Console.WriteLine("Alcoholica: " + cerveza.Alocoholic);
+        }
+    }
+}
